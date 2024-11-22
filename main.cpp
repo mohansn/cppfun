2def66e9522a0ab4a22a9a393019f5a977e69f7e
Mohan S N <mohansn@gmail.com>
2024-11-22 23:43:30 +0530

    added comments and examples

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <vector>
 #include <iostream>
 
+using std::vector;
+using std::cout;
+using std::endl;
+
 int process_int(int n)
 {
 	return n * 10;
@@ -11,7 +15,7 @@ int process_int(int n)
 int main ()
 {
 	auto print = [](int n) { return 0; };
-	std::vector<int> v {1,2,4,5};
+	vector<int> v {1,2,4,5};
 
 	/**
 		r is of type boost::optional<int>
@@ -19,16 +23,32 @@ int main ()
 		else it is a boost::optional<int> contains the value
 	*/
 	auto r = Find_if(begin(v), end(v), [](int n) { return n % 2 == 0; });
-
-	std::cout << r.map(process_int)
+	cout << r.map(process_int)
 		      .value_or(12345)
-		  << std::endl;
+		  << endl;
 
-	std::cout << r.map(print).value_or(12345) << std::endl;
+	cout << "\n################################" << endl;
+	cout << r.map(print).value_or(12345) << endl;
 
-	std::cout << "\n################################" << std::endl;
+	cout << "\n################################" << endl;
 	auto r2 = Find(begin(v), end(v), 0);
-	std::cout << r2.map(print).value_or(12345) << std::endl;
+	cout << r2.map(print).value_or(12345) << endl;
+
+	cout << "\n################################" << endl;
+	Find_if(begin(v), end(v),
+		[](int n) { return n > 0; }
+	       ).map(
+			/* Add your own lambda to run any operation on the value found */
+			[] (int n) {
+					cout << n << endl;	// for example
+					/*
+						unfortunately, we need this
+						because boost internally needs a reference of type T& where
+						T is the return type of the function passed to map().
+						Hence, void return is not possible.
+					*/
+					return 0;
+				   });
 	return 0; 
 }
 
